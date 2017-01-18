f517345de1d53ff68832ab179b6630d354583e4a
Hampus Eriksson <Hampus Eriksson>
2017-01-18 23:07:57 +0100

    Added more meat to functions and comments

--- lab1/Syscall_functions.c
+++ lab1/Syscall_functions.c
@@ -1,27 +1,40 @@
-void create(){
+
+/*Creates a new file with initial_size in bytes.
+Returns true if successfully done so, otherwise false.*/
+bool create(const char *file, unsigned initial_size){
 
 }
 
-void open(){
+/*Opens the given file and returns the file's fd or
+"File Descriptor. This is the a unique nonnegative 
+number*/
+int open(const char *file){
 
 }
 
-void close(){
+/*Closes the file descriptor given as parameter.*/
+void close(int fd){
 
 }
 
-void read(){
+/*Reads size bytes from an open file and returns number of
+bytes actually read.*/
+int read(int fd, void *buffer, unsigned size){
 
 }
 
-void write(){
+/*Tries to write size bytes from given buffer to an open file fd.
+Returns number of bytes actually written.*/
+int write(int fd, const void *buffer, unsigned size){
 
 }
 
-void halt(){
+/*Terminate Pintos by a call to power_off(). Use with cautions and 
+good knowledge as you lose information*/
+void halt(void){
 
 }
+/*Terminates the current user program. Returns status to kernel*/
+void exit(int status){
 
-void exit(){
-    
 }
